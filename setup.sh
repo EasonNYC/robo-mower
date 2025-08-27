30fd4dfdfd65fbb81bcd024fa5df4445aaac4488
Eason <EpicZero17@gmail.com>
2025-08-27 08:50:13 -0400

    Migrates to meta-ti layer
    Replaces meta-beagleboard with meta-ti in the build configuration.
    
    This change ensures compatibility with the latest Texas Instruments Yocto layer and aligns with current hardware support.

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ fi
 # Clone required repositories
 echo "Cloning Yocto layers..."
 git clone -b kirkstone git://git.yoctoproject.org/poky.git || echo "poky already exists"
-git clone -b kirkstone https://github.com/beagleboard/meta-beagleboard.git || echo "meta-beagleboard already exists"
+git clone -b kirkstone https://git.yoctoproject.org/meta-ti || echo "meta-ti already exists"
 git clone -b kirkstone https://github.com/mendersoftware/meta-mender.git || echo "meta-mender already exists"
 git clone -b kirkstone https://github.com/ros/meta-ros.git || echo "meta-ros already exists"
 git clone -b kirkstone https://git.openembedded.org/meta-openembedded || echo "meta-openembedded already exists"
@@ -32,17 +32,14 @@ source oe-init-build-env ../build
 
 # Copy configuration templates
 echo "Setting up configuration files..."
-if [ ! -f ../build/conf/local.conf ]; then
-    cp ../conf/local.conf.template ../build/conf/local.conf
-    echo "Created local.conf from template."
-fi
+mkdir -p build/conf
+cp conf/local.conf.template build/conf/local.conf
+cp conf/bblayers.conf.template build/conf/bblayers.conf
 
-if [ ! -f ../build/conf/bblayers.conf ]; then
-    cp ../conf/bblayers.conf.template ../build/conf/bblayers.conf
-    # Replace placeholders
-    sed -i "s|##OEROOT##|$(pwd)|g" ../build/conf/bblayers.conf
-    sed -i "s|##BUILDDIR##|$(pwd)/../build|g" ../build/conf/bblayers.conf
-fi
+# Replace placeholders in bblayers.conf
+PROJECT_ROOT=$(pwd)
+sed -i "s|##OEROOT##|${PROJECT_ROOT}/poky|g" build/conf/bblayers.conf
+sed -i "s|##BUILDDIR##|${PROJECT_ROOT}/build|g" build/conf/bblayers.conf
 
 echo "Setup complete!"
 echo "1. Edit secrets.env with Mender tenant token"
